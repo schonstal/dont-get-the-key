a24ad710b01ccbee45ed6e1f30cce75468ccf02a
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-10-09 23:06:41 +0000

    Should probably remember to svn add those files first (fleshed out input handler)

---
+++ DontGetTheKey/DontGetTheKey/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace DontGetTheKey
+{
+    public class Character : Actor
+    {
+        public Character(string actorName, SpriteBatch sb, ContentManager contentManager)
+            : base(actorName, sb, contentManager)
+        {
+            return;
+        }
+    }
+}
---
+++ DontGetTheKey/DontGetTheKey/InputHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace DontGetTheKey
+{
+    class InputHandler
+    {
+        private PlayerIndex player;
+        private GamePadState prev;
+
+        public InputHandler(PlayerIndex p1)
+        {
+            player = p1;
+        }
+
+        //We needn't worry about multibutton
+        public bool pressed(string button)
+        {
+            if ((buttonMap(button, GamePad.GetState(player)) == ButtonState.Pressed) && (buttonMap(button, prev) == ButtonState.Released))
+                return true;
+            return false;
+        }
+
+
+        public bool held(string button)
+        {
+            if (buttonMap(button, GamePad.GetState(player)) == ButtonState.Pressed)
+                return true;
+            return false;
+        }
+
+        //I'll have some dynamic programming, please. No? :(
+        private ButtonState buttonMap(string button, GamePadState state)
+        {
+            switch (button)
+            {
+                case "A":
+                    return state.Buttons.A;
+                case "B":
+                    return state.Buttons.B;
+                case "X":
+                    return state.Buttons.X;
+                case "Y":
+                    return state.Buttons.Y;
+                case "Back":
+                    return state.Buttons.Back;
+                case "BigButton":
+                    return state.Buttons.BigButton;
+                case "LeftShoulder":
+                    return state.Buttons.LeftShoulder;
+                case "RightShoulder":
+                    return state.Buttons.RightShoulder;
+                case "LeftStick":
+                    return state.Buttons.LeftStick;
+                case "RightStick":
+                    return state.Buttons.RightStick;
+                case "Start":
+                    return state.Buttons.Start;
+                default:
+                    return ButtonState.Released;
+            }
+        }
+    }
+}

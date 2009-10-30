57bd601ffb45549504fa6a0e26b130c9873124bc
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-10-30 01:01:39 +0000

    Character class incremental commit. Lots more fleshed out.  Moves slowly to the right

--- DontGetTheKey/DontGetTheKey/Character.cs
+++ DontGetTheKey/DontGetTheKey/Character.cs
@@ -17,6 +17,22 @@ namespace DontGetTheKey
     public class Character : Actor
     {
         Rectangle target;
+        float velocity = 30;
+        int frame = 0;
+
+        bool playerControlled = true;
+        bool walking = true;
+
+        enum State {
+            left,
+            right,
+            up,
+            down,
+            key
+        }
+
+        State state = State.right;
+
         public Character(string actorName, SpriteBatch sb, ContentManager contentManager,
             Vector2 pos, Texture2D texture, Rectangle box)
             : base(actorName, sb, contentManager, pos, texture, box)
@@ -25,9 +41,39 @@ namespace DontGetTheKey
             return;
         }
 
+        public override void Update(GameTime gameTime)
+        {
+            //I hate MSVS's curly brace on next line enforcement
+            if (walking == true)
+            {
+                //Next walking frame, then move the guy :smugdog:
+                frame = (frame + 1) % 2;
+                switch (state)
+                {
+                    case State.right:
+                        position.X += (velocity * (float)gameTime.ElapsedGameTime.Milliseconds)/1000;
+                        break;
+                    case State.left:
+                        position.X -= velocity * (float)gameTime.ElapsedGameTime.Seconds;
+                        break;
+                    case State.up:
+                        position.Y -= velocity * (float)gameTime.ElapsedGameTime.Seconds;
+                        break;
+                    case State.down:
+                        position.Y += velocity * (float)gameTime.ElapsedGameTime.Seconds;
+                        break;
+                }
+            }
+            else if (state != State.up && state != State.down)
+            {
+                frame = 0;
+            }
+        }
+
         public override void Draw(GameTime gameTime)
         {
             spriteBatch.Draw(sprite, position, target, Color.White);
         }
+
     }
 }

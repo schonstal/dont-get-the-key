265c18de46c41ec376b0cbf95370b473caa45fad
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-11-09 02:24:01 +0000

    added walking over, moved states into states dir

---
+++ DontGetTheKey/DontGetTheKey/States/WalkingOver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DontGetTheKey
+{
+    class WalkingOver : State
+    {
+        public WalkingOver(SpriteBatch sb, ContentManager contentManager, 
+            Dictionary<string, Actor> actors)
+            : base(sb, contentManager) {
+            this.actors = actors;
+        }
+    }
+}

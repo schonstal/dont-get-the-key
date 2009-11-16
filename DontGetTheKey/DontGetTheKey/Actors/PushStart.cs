87c1372bb94989aadfb4ed695522b68328818be1
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-11-16 01:03:30 +0000

    I should probably add PushStart.cs as well...

---
+++ DontGetTheKey/DontGetTheKey/Actors/PushStart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace DontGetTheKey
+{
+    class PushStart : Actor
+    {
+        bool drawText;
+        float fps = 2;
+        float elapsed;
+
+        public PushStart(SpriteBatch sb, ContentManager contentManager,
+            Vector2 pos, string texture, Rectangle box)
+            : base(sb, contentManager, pos, texture, box) {
+        }
+
+        public float Rate {
+            get { return fps; }
+            set { fps = value; }
+        }
+
+        public override void Update(GameTime gameTime) {
+            elapsed += gameTime.ElapsedGameTime.Milliseconds;
+            if (1000 / fps <= elapsed) {
+                drawText = !drawText;
+                elapsed = 0;
+            }
+        }
+
+        public override void Draw(GameTime gameTime) {
+            if (drawText)
+                spriteBatch.DrawString(ImageBank.Instance.font,
+                    "PUSH START BUTTON",
+                    position,
+                    Color.White);
+        }
+    }
+}

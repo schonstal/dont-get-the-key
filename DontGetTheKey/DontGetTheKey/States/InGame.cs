b843180ba92fa204af9107f0fbac94197f366dce
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-11-27 08:49:37 +0000

    Added boobs (chest)

--- DontGetTheKey/DontGetTheKey/States/InGame.cs
+++ DontGetTheKey/DontGetTheKey/States/InGame.cs
@@ -23,6 +23,17 @@ namespace DontGetTheKey
             ((Character)actors["main"]).PlayerControlled = true;
             actors.Remove("statcover");
             SoundBank.Instance.play("bgmusic", 1, 0, 0, true);
+
+            Register(
+                "chest",
+                new Actor(
+                    sb,
+                    contentManager,
+                    new Vector2(240, 136),
+                    "chest",
+                    new Rectangle(0, 0, 0, 0)
+                    )
+                );
         }
 
         public override void Update(GameTime gameTime) {

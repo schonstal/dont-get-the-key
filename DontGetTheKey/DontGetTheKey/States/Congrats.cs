7a3c1274ce75971f3d0da3995866d44547d306c3
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-12-04 08:22:38 +0000

    Adding missing source files to repository

---
+++ DontGetTheKey/DontGetTheKey/States/Congrats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace DontGetTheKey
+{
+    class Congrats : State
+    {
+        public Congrats(SpriteBatch sb, ContentManager contentManager, 
+            Dictionary<string, Actor> actors)
+            : base(sb, contentManager) {
+            this.actors = actors;
+            SoundBank.Instance.stop("bgmusic");
+            SoundBank.Instance.play("congrats", 1, 0, 0, true);
+            Register("grats", new Message(sb, contentManager, "CONGRATULATIONS!!!"));
+
+            ((Character)actors["main"]).PlayerControlled = false;
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ DontGetTheKey/DontGetTheKey/States/InventoryUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace DontGetTheKey
+{
+    class InventoryUp : State
+    {
+        public InventoryUp(SpriteBatch sb, ContentManager contentManager, 
+            Dictionary<string, Actor> actors)
+            : base(sb, contentManager) {
+            this.actors = actors;
+            SoundBank.Instance.play("pause");
+            foreach (KeyValuePair<string, Actor> kvp in actors)
+                kvp.Value.Tween(new Vector2(kvp.Value.Position.X, kvp.Value.Position.Y + 184), 1.5);
+
+            ((Character)actors["main"]).PlayerControlled = false;
+        }
+
+        public override void Update(GameTime gameTime) {
+              
+
+            base.Update(gameTime);
+        }
+    }
+}

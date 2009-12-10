94456ec27c1fa8bf427140ee6b2cbebc9f37cb1a
schonstal <schonstal@8775d402-e147-4b35-8a62-c8bad682c2a5>
2009-12-10 03:54:15 +0000

    Added Item class

---
+++ DontGetTheKey/DontGetTheKey/Actors/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace DontGetTheKey
+{
+    class Item : Actor
+    {
+        List<String> messages;
+        int msg = 0;
+
+        public Item(SpriteBatch sb, ContentManager contentManager, String texture, 
+            int slot, String name, List<String> messages)
+            : base(sb, contentManager, new Vector2(0,0), "item_" + texture, new Rectangle(0,0,0,0)) {
+            if (slot < 4) {
+                position.X += slot * 16;
+            } else {
+                position.Y += 16;
+                position.X += (slot - 4) * 16;
+            }
+            this.messages = messages;
+        }
+
+        public String Info {
+            get {
+                if (msg < messages.Count - 1)
+                    msg++;
+                return messages[msg];
+            }
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime) {
+            spriteBatch.Draw(ImageBank.Instance.texture(sprite), position, color);
+        }
+    }
+}

e71dc0989dbc9e24b9601bab763eb624c9fc3280
Josh Schonstal <josh@schonstal.org>
2010-12-07 14:29:09 -0800

    added HowToPlay.cs

---
+++ DontGetTheKey/DontGetTheKey/States/HowToPlay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace DontGetTheKey
+{
+    public class HowToPlay : State
+    {
+        float elapsed;
+        public HowToPlay(SpriteBatch sb, ContentManager contentManager)
+            : base(sb, contentManager) {
+
+            Register(
+                "controls",
+                new Actor(
+                    sb,
+                    content,
+                    new Vector2(0, 0),
+                    "controls",
+                    new Rectangle(0, 0, 0, 0)
+                    )
+                );
+        }
+
+        public override void Update(GameTime gameTime) {
+            elapsed += gameTime.ElapsedGameTime.Milliseconds;
+            //Can't loop over enums because no IEnumerable?
+            List<PlayerIndex> players = new List<PlayerIndex>() { PlayerIndex.One, PlayerIndex.Two, PlayerIndex.Three, PlayerIndex.Four };
+            foreach (PlayerIndex p in players)
+            {
+                InputHandler.Instance.Player = p;
+                if (InputHandler.Instance.pressed("Any"))
+                {
+                    GameState.Instance.Enter(new Intro(spriteBatch, content));
+                }
+            }
+
+            base.Update(gameTime);
+        }
+    }
+}

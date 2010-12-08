9bff19987072690a5b39e56ad3b35f48e5997d80
Josh Schonstal <josh@schonstal.org>
2010-12-08 11:44:47 -0800

    added Fader.cs

---
+++ DontGetTheKey/DontGetTheKey/Actors/Fader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace DontGetTheKey
+{
+    class Fader : Actor
+    {
+        //Rate for fade-in/out
+        int update;
+        //Percent opacity increase
+        float percent;
+
+        public Fader(SpriteBatch sb, ContentManager contentManager, int update, float percent)
+            : base(sb, contentManager, new Vector2(56,-136), "black", new Rectangle(0,0,0,0)) {
+                this.update = update;
+                this.percent = percent;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            elapsed += gameTime.ElapsedGameTime.Milliseconds;
+        }
+    }
+}
